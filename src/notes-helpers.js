5f7aaa6a08c99147bd22beaa223623c5cda1735d
Reif <bridgerrhammond@gmail.com>
2019-11-14 14:59:44 -0600

    final changes

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,14 +1,14 @@
 export const findFolder = (folders = [], folder_id) =>
-  folders.find(folder => folder.id == folder_id);
+  folders.find(folder => folder.id === folder_id);
 
 export const findNote = (notes, note_id) => {
-  return notes.find(note => note.id == note_id);
+  return notes.find(note => note.id === note_id);
 };
 
-// notes.find(note => note.id === note_id)
+// notes.find(note => note.id ==== note_id)
 
 export const getNotesForFolder = (notes = [], folder_id) =>
-  !folder_id ? notes : notes.filter(note => note.folder_id == folder_id);
+  !folder_id ? notes : notes.filter(note => note.folder_id === folder_id);
 
 export const countNotesForFolder = (notes = [], folder_id) =>
-  notes.filter(note => note.folder_id == folder_id).length;
+  notes.filter(note => note.folder_id === folder_id).length;
