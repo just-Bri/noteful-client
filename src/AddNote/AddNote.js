030debc8d616aa180aeee3efa847fb0682aa9e6a
Reif <bridgerrhammond@gmail.com>
2019-11-14 15:06:19 -0600

    final changes

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-import NotefulForm from '../NotefulForm/NotefulForm'
-import ApiContext from '../ApiContext'
-import config from '../config'
-import './AddNote.css'
+import React, { Component } from 'react';
+import NotefulForm from '../NotefulForm/NotefulForm';
+import ApiContext from '../ApiContext';
+import config from '../config';
+import './AddNote.css';
 
 export default class AddNote extends Component {
   static defaultProps = {
     history: {
-      push: () => { }
+      push: () => {},
     },
-  }
+  };
   static contextType = ApiContext;
 
   handleSubmit = e => {
@@ -19,68 +19,59 @@ export default class AddNote extends Component {
       content: e.target['note-content'].value,
       folder_id: e.target['note-folder-id'].value,
       date_added: new Date(),
-    }
+    };
     //console.log(newNote);
-    fetch(`${config.API_ENDPOINT}/notes`, {
+    fetch(`${config.API_ENDPOINT}/api/notes`, {
       method: 'POST',
       headers: {
-        'content-type': 'application/json'
+        'content-type': 'application/json',
       },
       body: JSON.stringify(newNote),
     })
       .then(res => {
-        if (!res.ok)
-          return res.json().then(e => Promise.reject(e))
-        return res.json()
+        if (!res.ok) return res.json().then(e => Promise.reject(e));
+        return res.json();
       })
       .then(note => {
-        this.context.addNote(note)
-        this.props.history.push(`/`)
+        this.context.addNote(note);
+        this.props.history.push(`/`);
       })
       .catch(error => {
-        console.error({ error })
-      })
-  }
+        console.error({ error });
+      });
+  };
 
   render() {
-    const { folders=[] } = this.context;
+    const { folders = [] } = this.context;
     //console.log(folders)
     return (
       <section className='AddNote'>
         <h2>Create a note</h2>
         <NotefulForm onSubmit={this.handleSubmit}>
           <div className='field'>
-            <label htmlFor='note-name-input'>
-              Name
-            </label>
+            <label htmlFor='note-name-input'>Name</label>
             <input type='text' id='note-name-input' name='note-name' />
           </div>
           <div className='field'>
-            <label htmlFor='note-content-input'>
-              Content
-            </label>
+            <label htmlFor='note-content-input'>Content</label>
             <textarea id='note-content-input' name='note-content' />
           </div>
           <div className='field'>
-            <label htmlFor='note-folder-select'>
-              Folder
-            </label>
+            <label htmlFor='note-folder-select'>Folder</label>
             <select id='note-folder-select' name='note-folder-id'>
               <option value={null}>...</option>
-              {folders.map(folder =>
+              {folders.map(folder => (
                 <option key={folder.id} value={folder.id}>
                   {folder.folder_name}
                 </option>
-              )}
+              ))}
             </select>
           </div>
           <div className='buttons'>
-            <button type='submit'>
-              Add note
-            </button>
+            <button type='submit'>Add note</button>
           </div>
         </NotefulForm>
       </section>
-    )
+    );
   }
 }
